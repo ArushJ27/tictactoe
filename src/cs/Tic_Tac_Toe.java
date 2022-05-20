bc1675a19cfe7fc209f8f21f5aea0503e1b36bb7
Arush <arush.dev.joshi@gmail.com>
2022-05-19 22:29:21 -0700

    refactored

--- src/cs/Tic_Tac_Toe.java
+++ src/cs/Tic_Tac_Toe.java
@@ -9,104 +9,101 @@ public class Tic_Tac_Toe {
 	private int[][] playerCol;
 	private int[][] playerDia;
 	private int turnNumber;
+	boolean hasWinner;
 
 	public Tic_Tac_Toe() {
 		board = new char[3][3];
-		for(char[] c: board)
+		for (char[] c : board)
 			Arrays.fill(c, ' ');
 		playerRows = new int[2][3];
 		playerCol = new int[2][3];
-		playerDia = new int [2][2];
+		playerDia = new int[2][2];
 		turnNumber = 0;
 		scan = new Scanner(System.in);
+		hasWinner = false;
+		play();
 	}
 
+	/**
+	 * 
+	 */
 	public void play() {
 		printBoard();
 		while (turnNumber < 9) {
+
 			updateBoard();
 			printBoard();
-			if(hasWinner()) {
-				
+			if (hasWinner) {
+
 				return;
 			}
-				
+			turnNumber++;
 		}
+		System.out.println("It's a tie");
 
 	}
-	//updates the board after every turn
+
+	/**
+	 * updates the board after every turn
+	 */
 	public void updateBoard() {
-		//checks who's turn it is
-		if(turnNumber % 2 == 0){
-			System.out.print("Player 1 enter a empty row and column seperated by a comma");
-		}
-		else {
-			System.out.print("Player 2 enter a empty row and column seperated by a comma");
-		}
-		try{
-			//get input and turns them into rows and columns
+		// checks who's turn it is
+		int playerNum = turnNumber % 2;
+		System.out.print("<Player" + (playerNum+1) + "> Enter a empty cell (row, col): ");
+		
+		try {
+			// get input and turns them into rows and columns
 			String s = scan.nextLine().trim();
 			String[] index = s.split(",");
-			//converts String input to usable index
-			int row = Integer.parseInt(index[0].trim())-1;
-			int col = Integer.parseInt(index[1].trim())-1;
-			//checks if valid
-			if(row<0 || row>2 || col < 0 || col > 2) {
-				System.out.println("not a valid index");
+			// converts String input to usable index
+			int row = Integer.parseInt(index[0].trim()) - 1;
+			int col = Integer.parseInt(index[1].trim()) - 1;
+			// checks if valid
+			if (row < 0 || row > 2 || col < 0 || col > 2) {
+				System.err.println("Not a valid index. Enter row/col in range 1 to 3.");
 				updateBoard();
 			}
-			if(board[row][col] != ' ') {
-				System.out.println("not a valid index");
+			if (board[row][col] != ' ') {
+				System.err.println("Cell not empty. Try again");
 				updateBoard();
 			}
-			//updates
-			if(turnNumber % 2 == 0)
+			// updates
+			if (playerNum == 0)
 				board[row][col] = 'X';
 			else
 				board[row][col] = 'O';
-			boolean isDia = false;
-			if((row == 0 && col == 0)||(row == 0 && col == 2)||(row == 1 && col == 1)||(row == 2 && col == 0)||(row == 2 && col == 2)) {
-				isDia = true;
+
+			playerRows[playerNum][row]++;
+			playerCol[playerNum][col]++;
+			if (row == col) {
+				playerDia[playerNum][0]++;
 			}
-			playerRows[turnNumber % 2][row]++;
-			playerRows[turnNumber % 2][col]++;
-			if(isDia)
-				
-				
-		
+			if (row + col == 2) {
+				playerDia[playerNum][1]++;
+			}
+			
+			//check if we have winner
+			if (playerRows[playerNum][row] == 3 || playerCol[playerNum][col] == 3 || playerDia[playerNum][0] == 3
+					|| playerDia[playerNum][1] == 3) {
+				System.out.println("Player "+(playerNum + 1) + " is the winner");
+				hasWinner = true;
+			}
+
 		}
-		//in case any user error
-		catch(Exception e) {
-			System.out.println("not a valid index");
+		// in case any user error
+		catch (Exception e) {
+			System.err.println("Not a valid index");
 			updateBoard();
 		}
-		
 
 	}
-	//prints board
+
+	// prints board
 	public void printBoard() {
-		for(char[] c: board) {
+		for (char[] c : board) {
 			System.out.println(Arrays.toString(c));
-		/*for (int i = 0; i < board.length; i++) {
-			for (int j = 0; j < board[i].length; j++) {
-				if (board[i][j] == ' ') {
-					System.out.print(" ");
-				} else {
-					System.out.print(board[i][j]);
-				}
-				if (j < board[i].length - 1) {
-					System.out.print("|");
-				}
-
-			}
-			System.out.println();
-			if (i < board.length - 1)
-				System.out.println("-----");*/
 		}
-	}
-
-	public boolean hasWinner() {
-
+		
 	}
 
 }
--- src/cs/homework.java
+++ src/cs/homework.java
@@ -1,10 +1,32 @@
 package cs;
 
+import java.util.*;
+
 public class homework {
+	static Scanner scan = new Scanner(System.in);
 
 	public static void main(String[] args) {
+		play();
+	}
+
+	public static void playAgain() {
+		System.out.print("Play again?(y/n):");
+		String s = scan.nextLine().trim();
+		if (s.equalsIgnoreCase("y")) {
+			play();
+		} else if (s.equalsIgnoreCase("n")) {
+			System.out.println("Thanks for playing");
+		} else {
+			System.err.println("Invalid input");
+			playAgain();
+
+		}
+
+	}
+
+	private static void play() {
 		Tic_Tac_Toe t = new Tic_Tac_Toe();
-		t.printBoard();
+		playAgain();
 	}
 
 }
